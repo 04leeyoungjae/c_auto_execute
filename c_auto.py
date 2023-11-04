01411c23b3298a00c3cf2d2b6c9c77082feabbdf
karinysis <leeyoungjae@pusan.ac.kr>
2023-11-04 23:06:18 +0900

    Add checking time

--- c_auto.py
+++ c_auto.py
@@ -5,6 +5,7 @@
 
 import subprocess
 import os
+import time
 
 def compile(filename):
     exe_filename="tmp.exe"
@@ -14,7 +15,7 @@ def compile(filename):
         compile_error=True
     return exe_filename, compile_error
 
-def run_c_file(filename,testcase):
+def run_c_file(filename,testcase,max_timeout=3):
     exe_filename,compile_error=compile(filename)
     outputs=[]
     frame="==============================\n"
@@ -22,21 +23,41 @@ def run_c_file(filename,testcase):
         output=frame+"Compile Error\n"+frame
         outputs.append(output)
         return outputs
+    num_exec=0
     for input_data in testcase:
-        output=frame
-        output+=f"입력값 : {input_data}\n출력값 : "
-        result=subprocess.run([exe_filename],input=input_data,stdout=subprocess.PIPE,text=True,universal_newlines=True)
-        if result.returncode!=0:
-            output+=(f"Error with return code {result.returncode}")
-        else:
-            output+=(result.stdout)
-        output+="\n"+frame
-        outputs.append(output)
+        num_exec+=1
+        input_str=input_data
+        if not input_data:
+            input_data="<!--void--!>"
+        output=frame+f"#{num_exec}\n\n"
+        output+=f"입력값\n{input_data}\n\n출력값\n"
+        try:
+            start=time.time()
+            result=subprocess.run([exe_filename],input=input_str,stdout=subprocess.PIPE,text=True,universal_newlines=True,timeout=max_timeout)
+            end=time.time()
+            if result.returncode!=0:
+                output+=(f"Error with return code {result.returncode}")
+            else:
+                if not result.stdout:
+                    output+="<!--void--!>"
+                else:
+                    output+=result.stdout
+            output+="\n\n"
+            output+=f"실행시간 : [{(end-start)*1000:.2f} ms]\n"+frame
+
+        except subprocess.TimeoutExpired:
+            output+=f"<!--Timeout--!>\n\n실행시간 : [Timeover {max_timeout*1000} ms]\n"+frame
+
+        finally:
+            outputs.append(output)
+
     os.remove(exe_filename)
     return outputs   
     
-
-filename="__test__.c"
-testcase=['','1','2']
-for output in run_c_file(filename,testcase):
-    print(output)
+if __name__=="__main__":
+    filename="__test__.c"
+    testcase=['','1','2']
+    with open("log.txt",'w',encoding="utf-8") as f:
+        for output in run_c_file(filename,testcase):
+            print(output)
+            f.write(output)
